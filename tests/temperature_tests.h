0784ffa47a12d807cb23508342e3aae40465a0ea
iainhemstock <iainhemstock@googlemail.com>
2018-04-30 09:53:12 +0100

    Refactor individual tests into single parameterized test

--- tests/temperature_tests.h
+++ tests/temperature_tests.h
@@ -5,28 +5,42 @@
 using namespace ::testing;
 #include "../inc/temperature_converters.h"
 
-TEST(CelsiusToFahrenheitTests, ExpectZeroToConvertToFahrenheit)
+class CelsiusToFahrenheitParamTests : public TestWithParam<std::pair<double, double>>
 {
-        EXPECT_THAT(
-                CelsiusToFahrenheitConverter().convert(0),
-                Eq(32.0)
-        );
-}
+public:
+        CelsiusToFahrenheitConverter cut;
+};
 
-TEST(CelsiusToFahrenheitTests, ExpectVerySmallNumberToConvertToFahrenheit)
-{
-        EXPECT_THAT(
-                CelsiusToFahrenheitConverter().convert(-3.14159265359e+8),
-                DoubleEq(-5.6548664564620006e+8)
-        );
-}
+INSTANTIATE_TEST_CASE_P(
+        ,
+        CelsiusToFahrenheitParamTests,
+        Values(
+                std::pair<double, double>(-3.14159265359e+8,   -5.6548664564620006e+8),
+                std::pair<double, double>(-50.0,               -58.0),
+                std::pair<double, double>(-40.0,               -40.0),
+                std::pair<double, double>(-30.0,               -22.0),
+                std::pair<double, double>(-20.0,               -4.0),
+                std::pair<double, double>(-10.0,               14.0),
+                std::pair<double, double>(0.0,                 32.0),
+                std::pair<double, double>(1.0,                 33.8),
+                std::pair<double, double>(2.0,                 35.6),
+                std::pair<double, double>(3.0,                 37.4),
+                std::pair<double, double>(4.0,                 39.2),
+                std::pair<double, double>(5.0,                 41.0),
+                std::pair<double, double>(10.0,                50.0),
+                std::pair<double, double>(20.0,                68.0),
+                std::pair<double, double>(30.0,                86.0),
+                std::pair<double, double>(40.0,                104.0),
+                std::pair<double, double>(50.0,                122.0),
+                std::pair<double, double>(3.14159265359e+8,    5.6548670964620006e+8)
+        )
+);
 
-TEST(CelsiusToFahrenheitTests, ExpectVeryLargeNumberToConvertToFahrenheit)
+TEST_P(CelsiusToFahrenheitParamTests, ShouldConvertCelsiusToFahrenheit)
 {
-        EXPECT_THAT(
-                CelsiusToFahrenheitConverter().convert(3.14159265359e+8),
-                DoubleEq(5.6548670964620006e+8)
-        );
+        const double celsius = GetParam().first;
+        const double fahrenheit = GetParam().second;
+        EXPECT_THAT(cut.convert(celsius), DoubleEq(fahrenheit));
 }
 
 TEST(FahrenheitToCelsiusTests, ExpectZeroToConvertToCelsius)
